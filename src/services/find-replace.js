409274e5cde851995beef07b2e39e05e4bbf991e
halildundar <halildundar.eee@gmail.com>
2025-05-26 14:27:48 +0300

    pwshell exe polciy

--- src/services/find-replace.js
+++ src/services/find-replace.js
@@ -278,7 +278,7 @@ async function replaceFolders(dest_folder) {
     process.cwd(),
     "sources/asansor/shell-scripts/find-replace.ps1"
   );
-  await cmd(`powershell.exe ${script_path} -noprofile -executionpolicy bypass -folderPath ${dest_folder}`);
+  await cmd(`powershell.exe -NoProfile -ExecutionPolicy Bypass ${script_path} -folderPath ${dest_folder}  -Verb RunAs`);
   return;
 }
 
@@ -324,6 +324,6 @@ async function findandReplaceFolder({ src_folder, dest_folder, kelimeler }) {
     );
     // console.log(kelimeler);
   }
-  await cmd(`powershell.exe ${script_path} -noprofile -executionpolicy bypass -folderPath ${dest_folder}`);
+  await cmd(`powershell.exe -NoProfile -ExecutionPolicy Bypass ${script_path}  -folderPath ${dest_folder}  -Verb RunAs`);
   console.log("This must happen last.");
 }
