b959fd10a79ee5a55511104b894df75feba64054
halildundar <halildundar.eee@gmail.com>
2025-05-27 06:31:51 +0300

    scri 1

--- src/services/find-replace.js
+++ src/services/find-replace.js
@@ -278,7 +278,7 @@ async function replaceFolders(dest_folder) {
     process.cwd(),
     "sources/asansor/shell-scripts/find-replace.ps1"
   );
-  await cmd(`powershell.exe ${script_path} -folderPath ${dest_folder}`);
+  await cmd(`powershell.exe -File ${script_path} -folderPath ${dest_folder}`);
   return;
 }
 //C:/Windows/System32/WindowsPowerShell/v1.0/
