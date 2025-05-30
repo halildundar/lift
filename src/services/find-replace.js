27182b7bad235f1e0dde55b35313dff533576727
halildundar <halildundar.eee@gmail.com>
2025-05-30 17:05:33 +0300

    dsads

--- src/services/find-replace.js
+++ src/services/find-replace.js
@@ -273,7 +273,7 @@ async function replaceFolders(dest_folder,recursive) {
     process.cwd(),
     "sources/asansor/shell-scripts/find-replace.ps1"
   );
-  await cmd(`powershell.exe -PassThru -Verb RunAs -WorkingDirectory ${dest_folder} -File ${script_path} -folderPath ${dest_folder} -IsRecursive ${recursive} `);
+  await cmd(`runas "powershell.exe   -File ${script_path} -folderPath ${dest_folder} -IsRecursive ${recursive}  -WorkingDirectory ${dest_folder}" `);
   return;
 }
 
